ff21ea46e221f00e3c63125696396509c6ee09da
Tetsuya Kitaoka <kitaoka.tetsuya.ks9@is.naist.jp>
2022-04-10 19:07:44 +0900

    [fish] add plugin

--- etc/install/fish.sh
+++ etc/install/fish.sh
@@ -11,5 +11,14 @@ type fish >/dev/null 2>&1
 if [[ $? -eq 1 ]] ; then
     echo "=> => install"
     brew install fish
+
+    echo "=> => [fisher] add plugin manager"
+    fish -c "curl -sL git.io/fisher | source && fisher install jorgebucaran/fisher"
+
+    echo "=> => [pure] add"
+    fish -c "fisher install pure-fish/pure"
+
+    echo "=> => [pyenv] add"
+    fish -c "fisher install daenney/pyenv"
 fi
 echo "=> [fish] complete"
